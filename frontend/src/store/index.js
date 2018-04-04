ca069e0500e62883d91aad94d0157e439fb5a111
Simon Park <simon.sungil.park@gmail.com>
2018-04-04 18:22:41 -0400

    added redux-devtools-extension package

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import reducers from '../modules';
 import ReduxThunk from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
   reducers,
-  applyMiddleware(ReduxThunk, promiseMiddleware())
+  composeWithDevTools(applyMiddleware(ReduxThunk, promiseMiddleware()))
 );
 
 export default store;
