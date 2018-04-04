6b3e05ec7882a92200e0660bce3d7da8485323b3
Simon Park <simon.sungil.park@gmail.com>
2018-04-04 18:36:32 -0400

    added fetchContacts reducer

--- frontend/src/modules/contacts.js
+++ frontend/src/modules/contacts.js
@@ -1,14 +1,45 @@
 import { createAction, handleActions } from 'redux-actions';
+import axios from 'axios';
 
 const FETCH_CONTACTS = 'simple-contacts/FETCH_CONTACTS';
+const FETCH_CONTACTS_PENDING = 'simple-contacts/FETCH_CONTACTS_PENDING';
+const FETCH_CONTACTS_FULFILLED = 'simple-contacts/FETCH_CONTACTS_FULFILLED';
+const FETCH_CONTACTS_REJECTED = 'simple-contacts/FETCH_CONTACTS_REJECTED';
 
-export const fetchContacts = createAction(FETCH_CONTACTS);
+export const fetchContacts = () => ({
+  type: FETCH_CONTACTS,
+  payload: axios.get('/api/contact')
+})
 
-const initialState = {};
+const initialState = {
+  pending: false,
+  error: false,
+  data: []
+};
 
 export default handleActions(
   {
-    [FETCH_CONTACTS]: (state, action) => state
+    [FETCH_CONTACTS_PENDING]: (state, action) => (
+      {
+        ...state,
+        pending: true,
+        error: false
+      }
+    ),
+    [FETCH_CONTACTS_FULFILLED]: (state, action) => (
+      {
+        ...state,
+        pending: false,
+        data: action.payload.data
+      }
+    ),
+    [FETCH_CONTACTS_REJECTED]: (state, action) => (
+      {
+        ...state,
+        pending: false,
+        error: true
+      }
+    )    
   },
   initialState
 );
