4b29217e3029480d3cbbb9e9739db17f5ee70f40
Simon Park <simon.sungil.park@gmail.com>
2018-04-05 22:30:26 -0400

    modified the style of ContactDetail

--- frontend/src/components/ContactDetail.js
+++ frontend/src/components/ContactDetail.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './ContactDetail.css'
+
+
 
 const ContactDetail = ({ contact }) => {
   return (
     <div className="container">
-      <p>{contact.firstname}</p>
-      <p>{contact.lastname}</p>
+      <div className="row">
+        <div className="mr-5" >
+          <h2 className="name">{`${contact.firstname} ${contact.lastname}`}</h2>
+        </div>
+        <div>
+          <button className="btn btn-info mr-2">
+            <i class="fas fa-edit"></i>
+          </button>
+          <button className="btn btn-danger">
+            <i class="fas fa-minus"></i>
+          </button>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-2 item-label">
+          Phone
+        </div>
+        <div className="col-10 item-content">
+          {contact.phone}
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-2 item-label">
+          Email
+        </div>
+        <div className="col-10 item-content">
+          {contact.email}
+        </div>
+      </div>
+
+      <button className="btn btn-secondary mt-5">
+        Back
+      </button>
+
     </div>
   )
 }
