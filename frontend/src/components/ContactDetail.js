bf7618c3d46b242531df9247437911bc208e248d
Simon Park <simon.sungil.park@gmail.com>
2018-04-06 17:46:45 -0400

    added confirm modal on detail component

--- frontend/src/components/ContactDetail.js
+++ frontend/src/components/ContactDetail.js
@@ -1,53 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './ContactDetail.css';
 import * as moment from 'moment';
+import { Modal } from 'reactstrap';
 
-const ContactDetail = ({ contact, showList, editContact, deleteContact }) => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h3 className="name my-1">{`${contact.firstname} ${contact.lastname}`}</h3>
-          <p className="updated">{'Updated ' + moment(contact.updated_at).fromNow()}</p>
-        </div>
-      </div>
 
-      <div className="row">
-        <div className="col-2 item-label">
-          Phone
-        </div>
-        <div className="col-10 item-content">
-          {contact.phone}
-        </div>
-      </div>
+class ContactDetail extends Component {
+  constructor() {
+    super();
 
-      <div className="row">
-        <div className="col-2 item-label">
-          Email
-        </div>
-        <div className="col-10 item-content">
-          {contact.email}
-        </div>
-      </div>
+    this.state = {
+      isOpenConfirm: false
+    };
+  }
 
-      <div className="mt-5">
-        <button className="btn btn-info mr-2" onClick={()=>editContact(contact.id)}>
-          <i className="fas fa-edit"></i> Edit
-        </button>
-        <button className="btn btn-danger mr-2" onClick={()=>deleteContact(contact.id)}>
-          <i className="fas fa-trash"></i> Delete
-        </button>
-      </div>
-      <div className="mt-4">
-        <button className="btn btn-secondary mr-2" onClick={()=>showList()} >
-        <i className="fas fa-chevron-left"></i> Back to List
-        </button>
-      </div>
+  openConfirm = () => {
+    this.setState({
+      isOpenConfirm: true
+    });
+  }
 
-    </div>
-  )
-}
+  closeConfirm = () => {
+    this.setState({
+      isOpenConfirm: false
+    });
+  }
+
+  confirmDelete = () => {
+    this.props.deleteContact(this.props.contact.id);
+  }
+
+  render() {
+    const { contact, showList, editContact } = this.props;
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h3 className="name my-1">{`${contact.firstname} ${contact.lastname}`}</h3>
+            <p className="updated">{'Updated ' + moment(contact.updated_at).fromNow()}</p>
+          </div>
+        </div>
+  
+        <div className="row">
+          <div className="col-2 item-label">
+            Phone
+          </div>
+          <div className="col-10 item-content">
+            {contact.phone}
+          </div>
+        </div>
+  
+        <div className="row">
+          <div className="col-2 item-label">
+            Email
+          </div>
+          <div className="col-10 item-content">
+            {contact.email}
+          </div>
+        </div>
+  
+        <div className="mt-5">
+          <button className="btn btn-info mr-2" onClick={()=>editContact(contact.id)}>
+            <i className="fas fa-edit"></i> Edit
+          </button>
+          <button className="btn btn-danger mr-2" onClick={this.openConfirm}>
+            <i className="fas fa-trash"></i> Delete
+          </button>
+        </div>
+        <div className="mt-4">
+          <button className="btn btn-secondary mr-2" onClick={()=>showList()} >
+          <i className="fas fa-chevron-left"></i> Back to List
+          </button>
+        </div>
+
+        <div>
+          <Modal isOpen={this.state.isOpenConfirm} toggle={this.closeConfirm} >
+            <div class="modal-body">
+              Are you sure?
+            </div>
+            <div class="modal-footer">
+              <button className="btn btn-primary mr-2" onClick={this.confirmDelete}>Yes</button>
+              <button className="btn btn-secondary" onClick={this.closeConfirm}>No</button>
+            </div>
+          </Modal>
+        </div>        
+      </div>
+    )
+  }
+} 
 
 ContactDetail.propTypes = {
   contact: PropTypes.shape({
