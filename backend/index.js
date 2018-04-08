9e5dcf83f230d2dacef9f683a427713570e62cce
Simon Park <simon.sungil.park@gmail.com>
2018-04-08 13:55:44 -0400

    added static folder setting up

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname+'/public'));
+
 app.use('/api/contact', require('./routes/ContactRoutes'));
 app.use('/api/image', require('./routes/ImageRoutes'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
